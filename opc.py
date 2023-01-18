08090c32c56abe55e59654f43a06690a25d4959d
sipocz <sipoczlaszlo@gmail.com>
2023-01-18 22:25:30 +0100

    opc server running

--- opc.py
+++ opc.py
@@ -1,20 +1,48 @@
-
-import pythoncom
-import pywintypes
-import win32com.client
-import win32com.server.util
+import sys
+sys.path.insert(0, "..")
 
 
+from opcua import Client
 
-import openopc2
-open_host='localhost'
-open_port='7766'
-from openopc2.da_client import OpcDaClient
-opc = OpcDaClient(open_host, open_port)
-opc.connect('Matrikon.OPC.Simulation.1')
 
-aux=opc.list('Simulation Items.Random')
-for item in aux:
-    print(item)
+if __name__ == "__main__":
 
-opc.close()
+    
+    client= Client("opc.tcp://desktop-bg17ih6:51310/CogentDataHub/DataAccess")
+    # client = Client("opc.tcp://admin@localhost:4840/freeopcua/server/") #connect using a user
+    try:
+        client.connect()
+
+        # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
+        root = client.get_root_node()
+        print("Objects node is: ", root)
+
+        # Node objects have methods to read and write node attributes as well as browse or populate address space
+        print("Children of root are: ", root.get_children())
+
+        # get a specific node knowing its node id
+        #var = client.get_node(ua.NodeId(1002, 2))
+        #var = client.get_node("ns=3;i=2002")
+        #print(var)
+        #var.get_data_value() # get value of node as a DataValue object
+        #var.get_value() # get value of node as a python builtin
+        #var.set_value(ua.Variant([23], ua.VariantType.Int64)) #set node value using explicit data type
+        #var.set_value(3.9) # set node value using implicit data type
+
+        # Now getting a variable node using its browse path
+        myvar = root.get_child(["Objects"])
+    
+        myvar_struct =client.get_node("ns=2;s=default:Simulation Items.Triangle Waves.Money")
+        before = myvar_struct.get_value()
+        #default=obj.get_child(["Simulation Items"])
+        print("myvar is: ", myvar)
+        print("myvar value  is: ", before)
+        #print("default is: ", default)
+        nodeid=client.get_namespace_array()
+        print(nodeid)
+
+        # Stacked myvar access
+        # print("myvar is: ", root.get_children()[0].get_children()[1].get_variables()[0].get_value())
+
+    finally:
+        client.disconnect()
